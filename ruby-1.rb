5de21fc41611a35eb231a25c9944d491e1d94f85
立石　純也 <dorasmapnogizaka46@tateishijunyanoMacBook-Air.local>
2020-10-11 14:08:10 +0900

    ruby_practice_day1

---
+++ ruby-1.rb
@@ -0,0 +1,25 @@
+puts "hello,world"
+
+puts 5
+
+puts "5+3"
+
+puts 5+3
+
+puts "I"+"like"+"nogizaka46"
+
+#文字から数字にするにはto＿i
+puts 100+"125".to_i
+
+#数字から文字にするにはto＿i
+puts "ドラえもんの声を"+26.to_s+" 年務めた大山のぶ代さんです。"
+
+puts "松村沙友理".length
+
+puts "なかだかな".reverse
+
+puts "酒豪すぎる久保さん".include?("酒豪")
+
+
+doraemon="大山のぶ代"
+puts doraemon
